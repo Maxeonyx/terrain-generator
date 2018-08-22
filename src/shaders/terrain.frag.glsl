a821c3be651b77fed65cd63765d751c09dc132be
maxeonyx <maxeonyx@gmail.com>
2018-08-22 20:47:46 +1200

    Implement texture blending and add support for a third texture. Currently has blue re-coloured lava as a placeholder.

--- src/shaders/terrain.frag.glsl
+++ src/shaders/terrain.frag.glsl
@@ -1,6 +1,6 @@
 #version 400
 
-const int N_TEX = 2;
+const int N_TEX = 3;
 
 uniform sampler2D tex_lava;
 uniform sampler2D tex_lavarock;
@@ -12,14 +12,19 @@ void main() {
 
     // take samples from each texture
     vec4 tex_sample[N_TEX];
-    tex_sample[0] = texture(tex_lava, tex_coord[0].xy) * tex_coord[0].z;
-    tex_sample[1] = texture(tex_lavarock, tex_coord[1].xy) * tex_coord[1].z;
 
-    // Add together textures
+    {
+        tex_sample[0] = texture(tex_lava, tex_coord[0].xy);
+        // Make lava a bit more orange
+        tex_sample[0] += vec4(0, 0.08, 0, 0);
+    }
+    tex_sample[1] = texture(tex_lavarock, tex_coord[1].xy);
+    tex_sample[2] = tex_sample[0].zyxw;
+
+    // Add together the samples and multiply by the weights
     vec4 color = vec4(0.0);
     for (int i = 0; i < N_TEX; i ++) {
-        color += tex_sample[i];
+        color += tex_sample[i] * tex_coord[i].z;
     }
-
     gl_FragColor = color;
 }
--- src/shaders/terrain.geom.glsl
+++ src/shaders/terrain.geom.glsl
@@ -9,21 +9,43 @@ uniform int which_heightmap;
 uniform sampler2D tex_heightmap;
 uniform sampler2D tex_heightmap_2;
 
-const int N_TEX = 2;
+const int N_TEX = 3;
 out vec3[N_TEX] tex_coord;
 
 void set_tex_coords(vec2 xy_lerp) {
 	tex_coord[0].xy = xy_lerp * 5;
 	tex_coord[1].xy = xy_lerp * 10;
+	tex_coord[2].xy = xy_lerp * 15;
 }
 
+const float ash_level = 0.7;
+const float ash_blend_range = 0.1;
+const float lava_level = 0.2;
+const float lava_blend_range = 0.1;
+
 void set_tex_weights(float z) {
-	if (z < 0.5) {
-		tex_coord[0].z = 1.0;
-		tex_coord[1].z = 0.0;
+	if (z > ash_level) {
+		tex_coord[0].z = 0;
+		tex_coord[1].z = 0;
+		tex_coord[2].z = 1;
+	} else if (z <= ash_level && z > (ash_level - ash_blend_range)) {
+		float lerp = (ash_level - z) / ash_blend_range;
+		tex_coord[0].z = 0;
+		tex_coord[1].z = lerp;
+		tex_coord[2].z = 1 - lerp;
+	} else if (z <= (ash_level - ash_blend_range) && z > (lava_level + lava_blend_range)) {
+		tex_coord[0].z = 0;
+		tex_coord[1].z = 1;
+		tex_coord[2].z = 0;
+	} else if (z <= (lava_level + lava_blend_range) && z > lava_level) {
+		float lerp = (z - lava_level) / lava_blend_range;
+		tex_coord[0].z = 1 - lerp;
+		tex_coord[1].z = lerp;
+		tex_coord[2].z = 0;
 	} else {
-		tex_coord[0].z = 0.0;
-		tex_coord[1].z = 1.0;
+		tex_coord[0].z = 1;
+		tex_coord[1].z = 0;
+		tex_coord[2].z = 0;
 	}
 }
 
